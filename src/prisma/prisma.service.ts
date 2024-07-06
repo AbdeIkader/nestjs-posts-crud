926205a02026b34b49a14b070ad46b9db01bda78
AbdeIkader <abdelrahmanabdelkader2002@gmail.com>
2024-07-06 15:38:30 +0300

    Setup PostgreSQL

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient
+  implements OnModuleInit {
+
+  constructor() {
+    super();
+  }
+
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
